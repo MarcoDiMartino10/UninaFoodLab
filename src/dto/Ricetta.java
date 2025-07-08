e8f4db26c60203486cd338e1ff0bf1ac9ee448f4
marco <marcodimartino47@gmail.com>
2025-07-08 16:00:52 +0200

    aggiornamento ricetta

--- src/dto/Ricetta.java
+++ src/dto/Ricetta.java
@@ -18,7 +18,7 @@ public class Ricetta {
 		this.nome = nome;
 		this.luogo = luogo;
 		this.orario_inizio = orario_inizio;
-		this.ingredienti = (ingredienti != null) ? ingredienti : new LinkedList<Ingrediente>();
+		this.ingredienti = ingredienti;
 	}
 	
 	// Getters
